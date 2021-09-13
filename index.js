d1acfa7a3b42f75cf6aeb542edcfe6b11219e158
xaviguasch <xaviguasch@gmail.com>
2021-09-13 12:15:23 +0200

    implemented slider logic

--- index.js
+++ index.js
@@ -17,3 +17,41 @@ const closeNavModal = () => {
 
 openBtn.addEventListener('click', openNavModal)
 closeBtn.addEventListener('click', closeNavModal)
+
+// Slider
+const slides = document.querySelectorAll('.slide')
+const leftSlideBtn = document.querySelector('#left-slide-btn')
+const rightSlideBtn = document.querySelector('#right-slide-btn')
+
+let i = 0
+
+const moveLeft = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('visible')
+  })
+
+  slides[i].classList.add('visible')
+
+  if (i <= 0) {
+    i = slides.length - 1
+  } else {
+    i--
+  }
+}
+
+const moveRight = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('visible')
+  })
+
+  slides[i].classList.add('visible')
+
+  if (i === slides.length - 1) {
+    i = 0
+  } else {
+    i++
+  }
+}
+
+leftSlideBtn.addEventListener('click', moveLeft)
+rightSlideBtn.addEventListener('click', moveRight)
